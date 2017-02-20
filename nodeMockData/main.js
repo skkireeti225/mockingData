3c9550f6de92b78dfa023c79a6586a4141fdc9c1
skkireeti225 <skkireeti225@gmail.com>
2017-02-19 21:44:06 -0800

    cross-origin

--- nodeMockData/main.js
+++ nodeMockData/main.js
@@ -28,6 +28,27 @@ if(files && files.length > 0) {
 
     var mapping = apiRoot + fileName.replace(mockRoot, '').replace(mockFilePattern,'');
 
+
+        // Add headers
+    app.use(function (req, res, next) {
+
+        // Website you wish to allow to connect
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
+
+        // Pass to next layer of middleware
+        next();
+    });
+
     app.get(mapping, function (req, res) {
       var data =  fs.readFileSync(fileName, 'utf8');
       res.writeHead(200, { 'Content-Type': 'application/json' });
